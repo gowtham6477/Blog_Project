a4b04e6b42a8841e0f103b57b774985cae68a7a5
gowtham6477 <jgjg6477@gmail.com>
2026-03-31 22:47:19 +0530

    configured admin panel

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,1 +1,46 @@
 from django.contrib import admin
+from django.utils import timezone
+
+from .models import Comment, Post, PostTag, Tag
+
+
+class PostTagInline(admin.TabularInline):
+	model = PostTag
+	extra = 0
+	autocomplete_fields = ["tag"]
+
+
+@admin.action(description="Publish selected posts")
+def publish_posts(modeladmin, request, queryset):
+	queryset.update(status=Post.Status.PUBLISHED, published_at=timezone.now())
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("title", "author", "status", "published_at", "views_count")
+	list_filter = ("status", "author", "published_at")
+	search_fields = ("title", "body")
+	prepopulated_fields = {"slug": ("title",)}
+	date_hierarchy = "published_at"
+	inlines = [PostTagInline]
+	actions = [publish_posts]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+	list_display = ("name", "slug")
+	search_fields = ("name",)
+	prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.action(description="Approve selected comments")
+def approve_comments(modeladmin, request, queryset):
+	queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ("name", "email", "post", "approved", "created_at")
+	list_filter = ("approved", "created_at")
+	search_fields = ("name", "email", "body")
+	actions = [approve_comments]
